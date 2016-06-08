7a9c860ebee7300de4dca4dc3f195daa7d67fdfc
Hugo Sanchez V <programacionhsv@gmail.com>
2016-06-08 17:53:23 -0500

    Arreglado puerto e ip

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express =  require('express');
 var app = express();
-server = app.listen(3000,'127.0.0.1',function(){
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP,
+	port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+	if (typeof ipaddress === "undefined") {
+		//  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+		//  allows us to run/test the app locally.
+		console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+		ipaddress = "127.0.0.1";
+	};
+
+server = app.listen(port,ipaddress,function(){
 	console.log('Server running');
 });
 
